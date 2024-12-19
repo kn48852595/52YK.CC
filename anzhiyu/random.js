6d750a460a75d67f8fe2b11f1a8c509fffa3b0ea
kn48852595 <48852595@qq.com>
2024-12-19 15:57:51 +0800

    Site updated: 2024-12-19 15:57:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/18/hello-world/"];function toRandomPost(){
+var posts=[":/abbrlink.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
